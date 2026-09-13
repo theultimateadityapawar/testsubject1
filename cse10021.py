84f73a7d7ccbecd5872f2de59f838a02921fba01
theultimateadityapawar <adityapawar1967@gmail.com>
2026-09-13 16:45:52 +0530

    this is the vityarthi project version_1

--- cse10021.py
+++ cse10021.py
@@ -1,19 +1,41 @@
 from datetime import datetime 
-n=int(input())
-z=int(input())
-x=int(input())
-v=int(input())
-s=int(input())
+n=int(input("enter the month"))
+z=int(input("enter the day"))
+x=int(input("enter the hour(24hour format)"))
+v=int(input("enter the minute"))
 current = datetime.now()
-target= datetime(2026, n, z, x, v, s)
-print(current)
-print(target)
-
+target= datetime(2026, n, z, x, v,)
+remaining_time= target - current
 bus_no=input("enter bus number") 
-for char in bus_no:
-    if char == bus_no[0]=="N" and char == bus_no[1]=="1":
-        print("bus is going north ",end="")
-
-        
-
-
+code=bus_no[:2]
+if code == "N2":
+    print("bus is going to DELHI",remaining_time)
+elif code == "N1":
+    print('bus is going to JAMMU', remaining_time)
+elif code == "N3":
+    print('bus is going to chandigarh',remaining_time)
+elif code == "N4":
+    print('bus is going to shimla', remaining_time)
+elif code == "E1":
+    print('bus is going to GHUWAHATI', remaining_time)
+elif code == 'E2':
+    print('bus is going to kolkata', remaining_time)
+elif code == 'E3':
+    print('bus is going to patna', remaining_time)
+elif code== 'S1':
+    print('bus is going to hydreabad', remaining_time)
+elif code== 'S2':
+    print('bus is going to bangalore', remaining_time)
+elif code =='S3':
+    print('bus is going to chennai', remaining_time)
+elif code =='W1':
+    print('bus is going to mumbai', remaining_time)
+elif code =='W2':
+    print('bus is going to ahemdabad', remaining_time)
+elif code =='W3':
+    print('bus is going to surat', remaining_time)
+elif code =='W4':
+    print('bus is going to jaipur', remaining_time)
+else:
+    print("bus is cancelled")
+    
