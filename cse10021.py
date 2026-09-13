22fce794ca49cb21ac22cc43fda291b49d5c70ae
theultimateadityapawar <adityapawar1967@gmail.com>
2026-09-13 14:16:32 +0530

    this is the project for vityarthi

--- cse10021.py
+++ cse10021.py
@@ -1,1 +1,19 @@
-print("hello world")
+from datetime import datetime 
+n=int(input())
+z=int(input())
+x=int(input())
+v=int(input())
+s=int(input())
+current = datetime.now()
+target= datetime(2026, n, z, x, v, s)
+print(current)
+print(target)
+
+bus_no=input("enter bus number") 
+for char in bus_no:
+    if char == bus_no[0]=="N" and char == bus_no[1]=="1":
+        print("bus is going north ",end="")
+
+        
+
+
