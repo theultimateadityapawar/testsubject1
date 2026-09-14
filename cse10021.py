7cfe925a479ccec1816501a4d6051755677bce4d
theultimateadityapawar <adityapawar1967@gmail.com>
2026-09-14 20:16:30 +0530

    vityarthi version 2

--- cse10021.py
+++ cse10021.py
@@ -6,36 +6,26 @@ v=int(input("enter the minute"))
 current = datetime.now()
 target= datetime(2026, n, z, x, v,)
 remaining_time= target - current
-bus_no=input("enter bus number") 
-code=bus_no[:2]
-if code == "N2":
-    print("bus is going to DELHI",remaining_time)
-elif code == "N1":
-    print('bus is going to JAMMU', remaining_time)
-elif code == "N3":
-    print('bus is going to chandigarh',remaining_time)
-elif code == "N4":
-    print('bus is going to shimla', remaining_time)
-elif code == "E1":
-    print('bus is going to GHUWAHATI', remaining_time)
-elif code == 'E2':
-    print('bus is going to kolkata', remaining_time)
-elif code == 'E3':
-    print('bus is going to patna', remaining_time)
-elif code== 'S1':
-    print('bus is going to hydreabad', remaining_time)
-elif code== 'S2':
-    print('bus is going to bangalore', remaining_time)
-elif code =='S3':
-    print('bus is going to chennai', remaining_time)
-elif code =='W1':
-    print('bus is going to mumbai', remaining_time)
-elif code =='W2':
-    print('bus is going to ahemdabad', remaining_time)
-elif code =='W3':
-    print('bus is going to surat', remaining_time)
-elif code =='W4':
-    print('bus is going to jaipur', remaining_time)
+bus_no = input("Enter bus number: ")
+code = bus_no[:2]
+routes = {
+    "N1": "JAMMU",
+    "N2": "DELHI",
+    "N3": "CHANDIGARH",
+    "N4": "SHIMLA",
+    "E1": "GUWAHATI",
+    "E2": "KOLKATA",
+    "E3": "PATNA",
+    "S1": "HYDERABAD",
+    "S2": "BANGALORE",
+    "S3": "CHENNAI",
+    "W1": "MUMBAI",
+    "W2": "AHMEDABAD",
+    "W3": "SURAT",
+    "W4": "JAIPUR"
+}
+if code in routes:
+    print("Bus is going to", routes[code])
+    print("Remaining time:", remaining_time)
 else:
-    print("bus is cancelled")
-    
+    print("Bus is cancelled")
